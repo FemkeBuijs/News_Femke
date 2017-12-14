97904fef3ce57080081666dbb6b209738fd10895
FemkeBuijs <32512274+FemkeBuijs@users.noreply.github.com>
2017-12-14 11:09:10 +0000

    Update for security

--- models/mongo_favourites.js
+++ models/mongo_favourites.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
+var globals = require('../models/globals.js');
 
-mongoose.connect('mongodb://dev:password@ds151355.mlab.com:51355/favourites');
+mongoose.connect('mongodb://'+globals.global.username+':'+globals.global.password+'@ds151355.mlab.com:51355/favourites');
 
 var Schema = mongoose.Schema;
 
